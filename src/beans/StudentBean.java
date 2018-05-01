62cfa292fbf973f4af9a99ef0f6c6200e4ff23db
Vakamisu <kammurimini@gmail.com>
2018-05-01 18:28:28 +0300

    added drop down list

--- src/beans/StudentBean.java
+++ src/beans/StudentBean.java
@@ -1,13 +1,23 @@
 package beans;
 
 import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean(name = "student")
 public class StudentBean {
     private String firstName;
     private String lastName;
+    private String country;
+    private List<String> countries;
 
     public StudentBean() {
+        countries = new ArrayList<>();
+        countries.add("Ukraine");
+        countries.add("US");
+        countries.add("Canada");
+        countries.add("Brazil");
+        countries.add("UK");
     }
 
     public String getFirstName() {
@@ -24,6 +34,18 @@ public class StudentBean {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<String> getCountries() {
+        return countries;
     }
 
     public String credits(){
